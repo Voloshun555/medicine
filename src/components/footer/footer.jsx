d3bd4fbfd93883fc05cd357afc72f81e1bb20313
Voloshun555 <Voloshunihor@gmail.com>
2024-03-09 19:16:39 +0200

    Update footer.jsx

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,12 +1,9 @@
-import { Link } from 'react-router-dom'
 import s from './footer.module.scss'
 
 export const Footer = () => {
     return (
       <div className={s.container}>
-        
-        <h1>Ігор Волошин</h1>
-        <Link>Git</Link>
+        <h1>Medicine Delivery app</h1>
       </div>
-  )
+    );
 }
